df4663935936433d61277a10ab2931e26f673f8b
Dionysen <solongnight@outlook.com>
2026-01-05 11:16:48 +0800

    移除TypeCache类及其相关方法的实现，注释掉相关代码以便后续参考

--- Coral.Native/Include/Coral/TypeCache.hpp
+++ Coral.Native/Include/Coral/TypeCache.hpp
@@ -1,33 +1,33 @@
-#pragma once
+// #pragma once
 
-#include "Core.hpp"
-#include "StableVector.hpp"
+// #include "Core.hpp"
+// #include "StableVector.hpp"
+// #include <unordered_map>
+// namespace Coral {
+// 	class Type;
 
-namespace Coral {
-	class Type;
+// 	class [[deprecated(CORAL_GLOBAL_ALC_MSG)]] TypeCache
+// 	{
+// 	public:
+// 		[[deprecated(CORAL_GLOBAL_ALC_MSG)]]
+// 		static TypeCache& Get();
 
-	class [[deprecated(CORAL_GLOBAL_ALC_MSG)]] TypeCache
-	{
-	public:
-		[[deprecated(CORAL_GLOBAL_ALC_MSG)]]
-		static TypeCache& Get();
+// 		[[deprecated(CORAL_GLOBAL_ALC_MSG)]]
+// 		Type* CacheType(Type&& InType);
 
-		[[deprecated(CORAL_GLOBAL_ALC_MSG)]]
-		Type* CacheType(Type&& InType);
+// 		[[deprecated(CORAL_GLOBAL_ALC_MSG_P(ManagedAssembly::GetLocalType))]]
+// 		Type* GetTypeByName(std::string_view InName) const;
 
-		[[deprecated(CORAL_GLOBAL_ALC_MSG_P(ManagedAssembly::GetLocalType))]]
-		Type* GetTypeByName(std::string_view InName) const;
+// 		[[deprecated(CORAL_GLOBAL_ALC_MSG)]]
+// 		Type* GetTypeByID(TypeId InTypeID) const;
 
-		[[deprecated(CORAL_GLOBAL_ALC_MSG)]]
-		Type* GetTypeByID(TypeId InTypeID) const;
+// 		[[deprecated(CORAL_GLOBAL_ALC_MSG)]]
+// 		void Clear();
 
-		[[deprecated(CORAL_GLOBAL_ALC_MSG)]]
-		void Clear();
+// 	private:
+// 		StableVector<Type> m_Types;
+// 		std::unordered_map<std::string, Type*> m_NameCache;
+// 		std::unordered_map<TypeId, Type*> m_IDCache;
+// 	};
 
-	private:
-		StableVector<Type> m_Types;
-		std::unordered_map<std::string, Type*> m_NameCache;
-		std::unordered_map<TypeId, Type*> m_IDCache;
-	};
-
-}
+// }
--- Coral.Native/Source/Coral/TypeCache.cpp
+++ Coral.Native/Source/Coral/TypeCache.cpp
@@ -1,40 +1,40 @@
-#include "Coral/TypeCache.hpp"
-#include "Coral/Type.hpp"
+// #include "Coral/TypeCache.hpp"
+// #include "Coral/Type.hpp"
 
-namespace Coral {
+// namespace Coral {
 
-	TypeCache& TypeCache::Get()
-	{
-		static TypeCache s_Instance;
-		return s_Instance;
-	}
+// 	TypeCache& TypeCache::Get()
+// 	{
+// 		static TypeCache s_Instance;
+// 		return s_Instance;
+// 	}
 
-	Type* TypeCache::CacheType(Type&& InType)
-	{
-		Type* type = &m_Types.Insert(std::move(InType)).second;
-		m_NameCache[type->GetFullName()] = type;
-		m_IDCache[type->GetTypeId()] = type;
-		return type;
-	}
+// 	Type* TypeCache::CacheType(Type&& InType)
+// 	{
+// 		Type* type = &m_Types.Insert(std::move(InType)).second;
+// 		m_NameCache[type->GetFullName()] = type;
+// 		m_IDCache[type->GetTypeId()] = type;
+// 		return type;
+// 	}
 
-	Type* TypeCache::GetTypeByName(std::string_view InName) const
-	{
-		auto name = std::string(InName);
-		auto it = m_NameCache.find(name);
-		return it != m_NameCache.end() ? it->second : nullptr;
-	}
+// 	Type* TypeCache::GetTypeByName(std::string_view InName) const
+// 	{
+// 		auto name = std::string(InName);
+// 		auto it = m_NameCache.find(name);
+// 		return it != m_NameCache.end() ? it->second : nullptr;
+// 	}
 
-	Type* TypeCache::GetTypeByID(TypeId InTypeID) const
-	{
-		auto it = m_IDCache.find(InTypeID);
-		return it != m_IDCache.end() ? it->second : nullptr;
-	}
+// 	Type* TypeCache::GetTypeByID(TypeId InTypeID) const
+// 	{
+// 		auto it = m_IDCache.find(InTypeID);
+// 		return it != m_IDCache.end() ? it->second : nullptr;
+// 	}
 
-	void TypeCache::Clear()
-	{
-		m_Types.Clear();
-		m_NameCache.clear();
-		m_IDCache.clear();
-	}
+// 	void TypeCache::Clear()
+// 	{
+// 		m_Types.Clear();
+// 		m_NameCache.clear();
+// 		m_IDCache.clear();
+// 	}
 
-}
+// }
